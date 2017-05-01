c662c75690425efa587cdcae1ef4ab2570fe80a3
victor335882 <victor335882@livemail.tw>
2017-05-02 07:30:18 +0800

    Lecture 52 Add search bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list';
-import BookDetail from '../containers/book-detail';
+import SearchBar from '../containers/search_bar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BookList/>
-        <BookDetail/>
+        <SearchBar/>
       </div>
     );
   }
