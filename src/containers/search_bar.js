fa5d2177274cf3d6132fc794e321cf85c96aede4
victor335882 <victor335882@livemail.tw>
2017-05-02 08:31:32 +0800

    Lecture 54 Prevent default submit
     onFormSubmit(event){
        event.preventDefault();
      }

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,16 +4,26 @@ export default class SearchBar extends Component{
   constructor(props){
     super(props);
     this.state = {term: ''};
-
     this.onInputChange = this.onInputChange.bind(this);
   }
   onInputChange(event){
-    console.log(event.target.value);
     this.setState({term:event.target.value});
+  }
+
+  {
+    //the <form> tag in html will let browser automatically
+    //sent the get and post request to server
+    //when you press the submit button
+    //and reset the value in input
+    //so we add this function to tell browser not to do this thing
+  }
+  onFormSubmit(event){
+    event.preventDefault();
+    // we need to go and fetch wether data
   }
   render() {
     return(
-      <from className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite sities"
           className="form-control"
@@ -22,7 +32,7 @@ export default class SearchBar extends Component{
         <span className = "input-group-btn">
           <button type = "submit" className = "btn btn-secondary">Submit</button>
         </span>
-      </from>
+      </form>
     )
   }
 }
