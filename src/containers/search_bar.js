43f0d20d22f1900d9a8f056cbec183db9c1ed59b
victor335882 <victor335882@livemail.tw>
2017-05-02 08:05:59 +0800

    Lecture 53  add onChange in input
    this.onInputChange = this.onInputChange.bind(this);
    
    can prevent the scope error of ‘this’

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
+    {
+      //this which is our instance of SearchBar
+      //has a function called onInputChange
+      //bind that function to this which is our instance of SearchBar
+      //and then replace onInputChange
+      //with the new bound instance of this
+      //store the value in this.onInputChange
+    }
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(event){
+    console.log(event.target.value);
+    this.setState({term:event.target.value});
+  }
   render() {
     return(
       <from className="input-group">
-        <input/>
+      {
+      //Due to the this here is not link to that this
+      //in function onInputChange(event)
+      //we need to add this.onInputChange = this.onInputChange.bind(this);
+      //in constructor to bind it
+      }
+        <input
+          placeholder="Get a five-day forecast in your favorite sities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}/>
         <span className = "input-group-btn">
           <button type = "submit" className = "btn btn-secondary">Submit</button>
         </span>
