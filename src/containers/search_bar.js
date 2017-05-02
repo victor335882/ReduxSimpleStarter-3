94133ca014309f81d0438b95328c2f6e146de99b
victor335882 <victor335882@livemail.tw>
2017-05-02 08:08:28 +0800

    Lecture 54.1
    delete previous comments

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,14 +4,7 @@ export default class SearchBar extends Component{
   constructor(props){
     super(props);
     this.state = {term: ''};
-    {
-      //this which is our instance of SearchBar
-      //has a function called onInputChange
-      //bind that function to this which is our instance of SearchBar
-      //and then replace onInputChange
-      //with the new bound instance of this
-      //store the value in this.onInputChange
-    }
+
     this.onInputChange = this.onInputChange.bind(this);
   }
   onInputChange(event){
@@ -21,12 +14,6 @@ export default class SearchBar extends Component{
   render() {
     return(
       <from className="input-group">
-      {
-      //Due to the this here is not link to that this
-      //in function onInputChange(event)
-      //we need to add this.onInputChange = this.onInputChange.bind(this);
-      //in constructor to bind it
-      }
         <input
           placeholder="Get a five-day forecast in your favorite sities"
           className="form-control"
