a050d951be6a70858e6082b076dffee31d1762a2
victor335882 <victor335882@livemail.tw>
2017-05-03 22:10:44 +0800

    Lecture 57.1
    delete previous commits

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-//install redux-promise to help us handle ajax requests
 import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-//insert (ReduxPromise) to hook a middleware into our redux process
-//middlleware is our function that take an action
-// and depanding on the action's type and the actions payload
-//orany other number of factors the middleware can choose to let
-//the action pass through. It can manipulate and modify the action
-//It can console.log or stop it, we can do all these
-//different types of little tasks on these actions before they reach any reducers
-//So you can kind of think of these middleware as gatekeepers of sorts
+
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
