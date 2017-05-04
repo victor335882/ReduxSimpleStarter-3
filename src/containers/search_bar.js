6a432b79eff72f91b2bf1f19d43694787fc2bfba
victor335882 <victor335882@livemail.tw>
2017-05-04 23:04:13 +0800

    Lecture 58.3
    if we add weather api, we need use
    http://api in the head
    
    hook to searchBar to our action by middleware

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
   constructor(props){
     super(props);
     this.state = {term: ''};
     this.onInputChange = this.onInputChange.bind(this);
+
+    //to make sure we will read correct props when submit
+    //we bind the this of onFormSubmit to the this of constructor
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
-  
+
   onInputChange(event){
     this.setState({term:event.target.value});
   }
 
   onFormSubmit(event){
     event.preventDefault();
+    //when user press submit button, it will return the term to fetchWeather
+    this.props.fetchWeather(this.state.term);
+    // And then reset the term to default value
+    this.setState({term:''});
   }
   render() {
     return(
@@ -29,3 +40,8 @@ export default class SearchBar extends Component{
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+export default connect(null, mapDispatchToProps)(SearchBar);
