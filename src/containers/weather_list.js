0c8d43d277899f1eb20d81d7a3e3c705096d09e2
victor335882 <victor335882@livemail.tw>
2017-05-06 14:46:35 +0800

    Lecture 63
    we will retrieve data from
    cityData.city and cityData.list
    
    install react sparkline to show the diagram
    
    add const temps to show the temperature

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,22 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import {Sparklines, SparklinesLine} from 'react-sparklines';
 class WeatherList extends Component {
   //add a function to retrive data of state,payload
-  // from Ajax request after start the action
+  //from Ajax request after start the action
+  //and this function is used to present a single city, a single row
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+
     return(
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red"/>
+          </Sparklines>
+        </td>
       </tr>
     )
   }
