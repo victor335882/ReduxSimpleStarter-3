b596f2c5d3dd517209841eb39d325988048e7193
victor335882 <victor335882@livemail.tw>
2017-05-06 10:20:29 +0800

    Lecture 61.1 delete previous comment and fix error

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,21 +1,10 @@
 import {FETCH_WEATHER} from '../actions/index';
 
-//due to we want to list our data
-//we initial our state as an array
+
 export default function(state = [], action) {
-  swith(action.type {
-    case FETCH_WEATHER;
-    //the rule in react is that only modify state by setState
-    //the state will be modify, if we do
-    //return state.push(action.payload.data);
-    //so we use state.concat to replace it
-    //to return a new state in following way
-    //return state.concat([action.payload.data]);
-    //we can write in ES6 syntax
-    return [ action.payload.data, ...state];
-    //make new array and put action.payload.data inside of it
-    //as first item, and list our origin state in second itenm
-    //and then return this new array to our state argument
+  swith (action.type) {
+    case FETCH_WEATHER:
+      return [ action.payload.data, ...state ];
   }
   return state;
 }
