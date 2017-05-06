06acb4e6f54abaa2577c0ffaa3487fec865a28b0
victor335882 <victor335882@livemail.tw>
2017-05-06 16:11:56 +0800

    Lecture 65 add the diagram of humidity and pressure

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,10 +1,19 @@
 import React, {Compoent} from 'react';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+import _ from 'lodash';
 
-export default (props) =>{
+export default (props) => {
+  function average(data){
+    return _.round(_.sum(data)/data.length);
+  }
+
   return(
-    <Sparklines height={120} width={180} data={props.data}>
-      <SparklinesLine color={props.color}/>
-    </Sparklines>
+    <div>
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color}/>
+        <SparklinesReferenceLine type = "avg"/>
+      </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
+    </div>
   )
-}
+};
