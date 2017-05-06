b36e5a6331854b0ae2d2f0f8b5c615cbfb31cbe2
victor335882 <victor335882@livemail.tw>
2017-05-06 08:46:12 +0800

    Lecture 59.3 promise hand ajax request
    add commit to explain promise which install in lecture 56

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,9 @@
 //reducer is just a function and the first argument is always our
 //state for the user is responsible for.
 //the second argument is always action
+//the action is the value returned by ../action/index.js
 export default function(state = null, action) {
+
   console.log('Action received', action);
   return state;
 }
