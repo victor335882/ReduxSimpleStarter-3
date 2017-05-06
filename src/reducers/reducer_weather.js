82c93d63c760be4476ab051cbab63e28f1b66eec
victor335882 <victor335882@livemail.tw>
2017-05-06 10:10:22 +0800

    Lecture 60 avoid state mutation
     return [ action.payload.data, ...state];
        //make new array and put action.payload.data inside of it
        //as first item, and list our origin state in second itenm
        //and then return this new array to our state argument

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,21 @@
-//reducer is just a function and the first argument is always our
-//state for the user is responsible for.
-//the second argument is always action
-//the action is the value returned by ../action/index.js
-export default function(state = null, action) {
+import {FETCH_WEATHER} from '../actions/index';
 
+//due to we want to list our data
+//we initial our state as an array
+export default function(state = [], action) {
+  swith(action.type {
+    case FETCH_WEATHER;
+    //the rule in react is that only modify state by setState
+    //the state will be modify, if we do
+    //return state.push(action.payload.data);
+    //so we use state.concat to replace it
+    //to return a new state in following way
+    //return state.concat([action.payload.data]);
+    //we can write in ES6 syntax
+    return [ action.payload.data, ...state];
+    //make new array and put action.payload.data inside of it
+    //as first item, and list our origin state in second itenm
+    //and then return this new array to our state argument
+  }
   return state;
 }
